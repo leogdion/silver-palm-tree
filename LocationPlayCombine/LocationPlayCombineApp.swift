0775b6879643e33ca443517f64c62ab890ee25fc
Leo Dion <leogdion@brightdigit.com>
2022-01-07 16:29:01 -0500

    Initial Commit

---
+++ LocationPlayCombine/LocationPlayCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPlayCombineApp.swift
+//  LocationPlayCombine
+//
+//  Created by Leo Dion on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationPlayCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
